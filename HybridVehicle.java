2dd286fc1eda1581c26a3e33bb502ff8e4cc4ff3
tommyxiao19491001 <348829664@tdsb.ca>
2026-05-28 10:55:20 -0400

    Add files via upload

---
+++ HybridVehicle.java
@@ -0,0 +1,13 @@
+/* 
+File Name: HybridVehicle.java
+Name: Tommy Xiao
+Class: ICS4U1-21
+Date: May 28, 2026
+Description: The HybridVehicle class.
+*/
+public class HybridVehicle extends Vehicle{
+
+
+
+
+}
