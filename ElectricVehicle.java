1d390075cec2dbf59de491b140860c63e50a647c
tommyxiao19491001 <348829664@tdsb.ca>
2026-05-28 10:55:47 -0400

    Add files via upload

---
+++ ElectricVehicle.java
@@ -0,0 +1,13 @@
+/* 
+File Name: ElectricVehicle.java
+Name: Tommy Xiao
+Class: ICS4U1-21
+Date: May 28, 2026
+Description: The ElectricVehicle class.
+*/
+public class ElectricVehicle extends Vehicle{
+
+
+
+
+}
---
+++ GasVehicle.java
@@ -0,0 +1,13 @@
+/* 
+File Name: GasVehicle.java
+Name: Tommy Xiao
+Class: ICS4U1-21
+Date: May 28, 2026
+Description: The GasVehicle class.
+*/
+public class GasVehicle extends Vehicle{
+
+
+
+
+}
